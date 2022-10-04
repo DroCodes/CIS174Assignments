2cc2152efee9576a17f8d38c2479e2866b351d6d
DroCodes <101131393+DroCodes@users.noreply.github.com>
2022-10-03 19:16:43 -0500

    Add project files.

---
+++ Controllers/Assignment6-1Controller.cs
@@ -0,0 +1,21 @@
+﻿using CIS174AssignmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174AssignmentApp.Controllers
+{
+    public class Assignment6_1Controller : Controller
+    {
+        public IActionResult Index(int accessLevel)
+        {
+            List<StudentModel> students = new List<StudentModel>();
+
+            students.Add(new StudentModel { FirstName = "John", LastName = "Smith", Grade = "A" });
+            students.Add(new StudentModel { FirstName = "Alice", LastName = "Williams", Grade = "A" });
+            students.Add(new StudentModel { FirstName = "Ryan", LastName = "Maddison", Grade = "C" });
+
+            ViewBag.userAccess = accessLevel;
+
+            return View(students);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using CIS174AssignmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CIS174AssignmentApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
